16103881a6410883f9c07ed9a6a6fdc021937245
Trevor Reznick <electrics.sheeps@gmail.com>
2025-02-01 08:56:56 +0100

    OPF-84 set manifest, env

---
+++ modules/config/env.js
@@ -0,0 +1,11 @@
+/* @@ config @@ */
+
+export const api_url_prod = 'https://openfav-dev.vercel.app'
+export const api_url_dev = 'http://localhost:4321'
+//const _url_old = 'https://bookmarks-list.netlify.app'
+export const _url = api_url_prod
+export const sessionAuthUrl = _url + '/api/v1/auth/signin'
+export const YOUR_TOKEN = '89773db3-7863-460c-ad3c-6abd0db43f1c'
+export const API_URL = 'https://vnavarra.nuvolaris.dev/api/my/openfavs/classify?url='
+export const NODE_REDIS_API_URL = 'https://openfav-node.fly.dev/api'
+export const REDIS_SESSION_API_URL = `${NODE_REDIS_API_URL}/tokens`
--- modules/main.js
+++ modules/main.js
@@ -1,12 +1,14 @@
 // modules/main.js
 import { greet, getRandomColor } from './dom.js'
 import { logger, showState } from './utils/utils.js'
+import * as cnf from './config/env.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     
     const greetButton = document.getElementById('greetButton')
     const greetingElement = document.getElementById('greeting')
     logger.log('welcome')
+    logger.log('test read cnf', cnf.API_URL)
 
     greetButton.addEventListener('click', () => {
         const greeting = greet('World')
